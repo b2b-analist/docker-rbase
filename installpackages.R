cbbea6c07af22a9ecba0df81371e183502e380c8
B2B.Web.ID Analist <pengelola.b2b@gmail.com>
2016-10-11 10:33:37 +0700

    add repo

--- installpackages.R
+++ installpackages.R
@@ -1,3 +1,8 @@
+local({
+  old <- getOption("defaultPackages"); r <- getOption("repos")
+  r["CRAN"] <- "http://cran.repo.bppt.go.id/"
+  options(defaultPackages = c(old, "MASS"), repos = r)
+})
 update.packages(ask=FALSE)
 install.packages(c(
  'ggplot2',
